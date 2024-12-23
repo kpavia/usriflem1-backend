079d2205e31e529aade1881f2527235522321d5e
Kenny <kenny@monumetric.com>
2024-12-22 17:25:47 -0700

    Fix another thing

--- src/core/wsgi.py
+++ src/core/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
