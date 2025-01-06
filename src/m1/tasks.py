4596e99fadd40923c5330b2c362f1add8c732e21
Kenny <kenny@monumetric.com>
2025-01-05 22:37:48 -0700

    Remove op rod many to many field; add unit test

--- src/m1/tasks.py
+++ src/m1/tasks.py
@@ -1,4 +1,17 @@
-from m1.models import Receiver
+from m1.models import Receiver, OpRod
+
+
+def get_op_rod(sn, maker):
+    op_rods = OpRod.objects.all()
+    ww2_rods = op_rods.filter(maker=maker, starting_serial__isnull=False, ending_serial__isnull=False)
+    possible_op_rods = list()
+    for rod in ww2_rods:
+        if sn >= rod.starting_serial and sn <= rod.ending_serial:
+            possible_op_rods.append({
+                'drawing_number': rod.drawing_number,
+                'sn_range': f'{rod.starting_serial} - {rod.ending_serial}'
+            })
+    return possible_op_rods
 
 
 def rifle_data(data):
--- src/m1/tests.py
+++ src/m1/tests.py
@@ -1,44 +1,184 @@
 from django.test import TestCase
-from m1.models import Receiver
+from m1.models import (
+    Receiver,
+    OpRod
+)
+from m1.tasks import rifle_data, get_op_rod
 
 
-class SerialNumberDatingTests(TestCase):
+class CustomUnitTests:
 
-    def setUp(self):
-        self.sn1 = 10
-        self.sn1_month = 'August'
-        self.sn1_year = 1937
-
-        self.sn2 = 300
-        self.sn2_month = 'September'
-        self.sn2_year = 1937
-
-        self.sn3 = 1000
-        self.sn3_month = 'December'
-        self.sn3_year = 1937
+    def __init__(self):
+        self.test_sn1 = 971531
+        self.test_sn2 = 11358
+        self.test_sn3 = 345121
+        self.test_sn4 = 566924
+        self.test_sn5 = 2154846
+        self.test_sn6 = 3413278
+        self.receivers = Receiver.objects.all()
+        self.op_rods = OpRod.objects.filter(
+            maker='SA',
+            starting_serial__isnull=False,
+            ending_serial__isnull=False
+        )
     
+    def test_receiver_date(self):
+        # test 1
+        receiver = rifle_data({'serial_number': self.test_sn1, 'maker': 'SA'})
 
-    def get_receiver_date(self, maker, serial_number):
-        receiver = Receiver.objects.filter(maker=maker, starting_serial__lte=serial_number, ending_serial__gte=serial_number).first()
-        return receiver
+        try:
+            assert isinstance(receiver, dict)
+            assert receiver.get('month', '') == 'November'
+            assert receiver.get('year', 0) == 1942
+        except AssertionError as ae:
+            raise ae
+        else:
+            print('Unit tests 1 passed')
 
-        # for receiver in receivers:
-        #     if serial_number >= receiver.starting_serial and serial_number <= receiver.ending_serial:
-        #         return (receiver.month, receiver.year)
-    
-
-    def test_sa_sn_dates(self):
-        maker = 'SA'
+        # negative test
+        try:
+            assert receiver.get('month', '') == 'September'
+            assert receiver.get('year', 0) == 1943
+        except AssertionError as ae:
+            print('Negative tests for test 1 passed')
+        else:
+            raise AssertionError('Negative tests 1 failed')
         
-        sn1_receiver = self.get_receiver_date(maker, self.sn1)
-        self.assertEqual(sn1_receiver.month, self.sn1_month)
-        self.assertEqual(sn1_receiver.year, self.sn1_year)
+        # test 2
+        receiver = rifle_data({'serial_number': self.test_sn2, 'maker': 'SA'})
+        try:
+            assert isinstance(receiver, dict)
+            assert receiver.get('month', '') == 'May'
+            assert receiver.get('year', 0) == 1939
+        except AssertionError as ae:
+            raise ae
+        else:
+            print('Unit tests 2 passed')
 
-        sn2_receiver = self.get_receiver_date(maker, self.sn2)
-        self.assertEqual(sn2_receiver.month, self.sn2_month)
-        self.assertEqual(sn2_receiver.year, self.sn2_year)
+        # negative test
+        try:
+            assert receiver.get('month', '') == 'April'
+            assert receiver.get('year', 0) == 1937
+        except AssertionError as ae:
+            print('Negative tests for test 2 passed')
+        else:
+            raise AssertionError('Negative test 2 failed')
+        
 
-        sn3_receiver = self.get_receiver_date(maker, self.sn3)
-        self.assertEqual(sn3_receiver.month, self.sn3_month)
-        self.assertEqual(sn3_receiver.year, self.sn3_year)
+        # test 3
+        receiver = rifle_data({'serial_number': self.test_sn3, 'maker': 'SA'})
+        try:
+            assert isinstance(receiver, dict)
+            assert receiver.get('month', '') == 'September'
+            assert receiver.get('year', 0) == 1941
+        except AssertionError as ae:
+            raise ae
+        else:
+            print('Unit tests 3 passed')
 
+        # negative test
+        try:
+            assert receiver.get('month', '') == 'April'
+            assert receiver.get('year', 0) == 1937
+        except AssertionError as ae:
+            print('Negative tests for test 3 passed')
+        else:
+            raise AssertionError('Negative tests 3 failed')
+
+        # test 4
+        receiver = rifle_data({'serial_number': self.test_sn4, 'maker': 'SA'})
+        try:
+            assert isinstance(receiver, dict)
+            assert receiver.get('month', '') == 'April'
+            assert receiver.get('year', 0) == 1942
+        except AssertionError as ae:
+            raise ae
+        else:
+            print('Unit tests 4 passed')
+
+        # negative test
+        try:
+            assert receiver.get('month', '') == 'April'
+            assert receiver.get('year', 0) == 1937
+        except AssertionError as ae:
+            print('Negative tests for test 4 passed')
+        else:
+            raise AssertionError('Negative tests 4 failed')
+        
+        # test 5
+        receiver = rifle_data({'serial_number': self.test_sn5, 'maker': 'SA'})
+        try:
+            assert isinstance(receiver, dict)
+            assert receiver.get('month', '') == 'November'
+            assert receiver.get('year', 0) == 1943
+        except AssertionError as ae:
+            raise ae
+        else:
+            print('Unit tests 5 passed')
+
+        # negative test
+        try:
+            assert receiver.get('month', '') == 'April'
+            assert receiver.get('year', 0) == 1937
+        except AssertionError as ae:
+            print('Negative tests for test 5 passed')
+        else:
+            raise AssertionError('Negative tests 5 failed')
+        
+        # test 6
+        receiver = rifle_data({'serial_number': self.test_sn6, 'maker': 'SA'})
+        try:
+            assert isinstance(receiver, dict)
+            assert receiver.get('month', '') == 'January'
+            assert receiver.get('year', 0) == 1945
+        except AssertionError as ae:
+            raise ae
+        else:
+            print('Unit tests 6 passed')
+
+        # negative test
+        try:
+            assert receiver.get('month', '') == 'April'
+            assert receiver.get('year', 0) == 1937
+        except AssertionError as ae:
+            print('Negative tests for test 6 passed')
+        else:
+            raise AssertionError('Negative tests 6 failed')
+
+
+    def test_op_rod(self):
+        # test 1
+        rod_1 = get_op_rod(self.test_sn1, 'SA')
+        possible_rods = [
+            'D35382 3SA',
+            'D35382 6 SA',
+            'D35382 8 SA'
+        ]
+        assert isinstance(rod_1, list)
+        for rod in rod_1:
+            try:
+                print(rod)
+                assert rod.get('drawing_number', '') in possible_rods
+            except AssertionError:
+                print(f'S/N: {self.test_sn1}')
+                print(f'{rod.get("drawing_number", "")} not in possible rods')
+                raise AssertionError
+            else:
+                print('Test 1 passed')
+        
+        # test 2
+        rod_2 = get_op_rod(self.test_sn2, 'SA')
+        possible_rods = [
+            'D 35382-0',
+        ]
+        assert isinstance(rod_2, list)
+        for rod in rod_2:
+            try:
+                assert rod.get('drawing_number', '') in possible_rods
+            except AssertionError:
+                print(f'S/N: {self.test_sn2}')
+                print(f'{rod.get("drawing_number", "")} not in possible rods')
+                raise AssertionError
+            else:
+                print('Test 2 passed')
+ 
