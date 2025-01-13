475a19c0acaef6161a02747b9327a480982f533b
Kenny <kenny@monumetric.com>
2025-01-12 23:10:34 -0700

    Bolt model

--- src/m1/models.py
+++ src/m1/models.py
@@ -31,3 +31,19 @@ class OpRod(models.Model):
 
     def __str__(self):
         return f'{self.maker} {self.drawing_number}'
+
+
+class Bolt(models.Model):
+    MAKER_CHOICES = {
+        'SA': 'Springfield Armory',
+        'W': 'Winchester Repeating Arms',
+        'IHC': 'International Harvester',
+        'HR': 'Harrington & Richardson'
+    }
+    maker = models.CharField(max_length=32, choices=MAKER_CHOICES)
+    drawing_number = models.CharField(max_length=30)
+    starting_serial = models.PositiveIntegerField(null=True, blank=True)
+    ending_serial = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.maker} bolt {self.drawing_number}'
